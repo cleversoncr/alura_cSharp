6aeaeefe63b27d8d98b29aa9d419c520e5d943b9
Cleverson Rocha <cleverson.rocha@minu.co>
2022-02-09 23:18:32 -0300

    Continuaçao do projeto C#

---
+++ 02_criandoVariaveis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace _02_criandoVariaveis
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Executando projeto 2 - Criando Variaveis");
+
+      int idade;
+      idade = 43;
+      Console.WriteLine(idade);
+
+      idade = 10;
+      Console.WriteLine(idade);
+
+      idade = 10 + 5;
+      Console.WriteLine(idade);
+
+      idade = (10 + 5) * 2;
+      Console.WriteLine("Sua idade é " + idade + "!");
+
+      Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+      Console.ReadLine();
+    }
+  }
+}
---
+++ 03_criandoVariaveis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Olá novo teste");
+
+    int idade = 43;
+    Console.WriteLine("Minha idade é " + idade + "!");
+
+    idade += 10;
+    Console.WriteLine(idade);
+
+    idade -= 50;
+    Console.WriteLine(idade);
+
+    idade *= 2;
+    Console.WriteLine(idade);
+
+    idade /= 2;
+    Console.WriteLine(idade);
+  }
+}
