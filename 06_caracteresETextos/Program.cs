b6d55f18ba616a6b8ab806be7ed469844c78677a
Cleverson Rocha <cleverson.rocha@minu.co>
2022-02-14 10:09:56 -0300

    Atribuindo variaveis

---
+++ 06_caracteresETextos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Executando o projeto 5 - Caracteres e textos");
+
+    //character
+    char primeiraLetra = 'a';
+    Console.WriteLine(primeiraLetra);
+
+    primeiraLetra = (char)65;
+    Console.WriteLine(primeiraLetra);
+
+    primeiraLetra = (char)(primeiraLetra + 1);
+    Console.WriteLine(primeiraLetra);
+
+    string titulo = $"Alura Cursos de tecnologia {2020}";
+    string cusosProgramacao = " - .NET" +
+    " - Java" +
+    " - JavaScript";
+
+    string cusosProgramacao2 =
+@"- .NET
+- Java
+- JavaScript";
+
+    Console.WriteLine(titulo);
+    Console.WriteLine(cusosProgramacao);
+    Console.WriteLine(cusosProgramacao2);
+
+
+    Console.ReadLine();
+  }
+}
---
+++ 07_atribuicaoDeVariaveis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Executando o projeto 6 - atribuicao de variaveis");
+
+    int idade = 43;
+    int idadeCleverson = idade;
+
+    idade = 20;
+
+    Console.WriteLine(idade);
+    Console.WriteLine(idadeCleverson);
+
+    Console.ReadLine();
+  }
+}
